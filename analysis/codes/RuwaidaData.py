5ee6c9c9b352b19f914a7b31f5ccba68cc81fbd6
Ruwaida Rashid <RURARONG@GMAIL.COM>
2023-04-13 15:29:59 -0700

    Made changes chaining

--- analysis/codes/RuwaidaData.py
+++ analysis/codes/RuwaidaData.py
@@ -3,10 +3,14 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def filter_dataset(file_path,mydata):
-    mydata = (pd.read_csv(file_path)
-              .dropna(subset=['distance_from_home'])
-              .dropna(axis=1, how='any')
-              .query('distance_from_home < 15000 & ((used_pin_number == 1) | (used_chip == 1))')
-              .iloc[:len(mydata) // 65])
-    return mydata
+import pandas as pd
+
+def filter_data(file_path,df):
+    df = (
+        pd.read_csv(file_path)
+        .dropna(subset=['distance_from_home'])
+        .iloc[:len(df) // 65]
+        .query('(distance_from_home < 15000) & (used_pin_number == 1) | (used_chip == 1)')
+        .dropna(axis=1, how='any')
+    )
+    return df
